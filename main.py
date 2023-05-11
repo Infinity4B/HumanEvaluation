26c2d0f0d6478271419695e1d2640749d6dd65bd
Infinity4B <mail707293768@qq.com>
2023-05-12 00:53:27 +0800

    Update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
+# from flask_paginate import Pagination, get_page_parameter
 app = Flask(__name__)
 
-TOTAL_NUM=20
+TOTAL_NUM=200
+INDEX_PAGES=TOTAL_NUM//36
 
 @app.route('/')
 def mainpage():
@@ -10,8 +12,9 @@ def mainpage():
 
 @app.route('/<num>')
 def rating_n(num):
+   page = int(request.args.get('page', 1))
    file = './static/1.png'
-   return render_template('./index.html',file=file,num=num,totalNum=TOTAL_NUM)
+   return render_template('./index.html',file=file,num=num,totalNum=TOTAL_NUM,page=page)
 
 if __name__ == '__main__':
    app.run(debug=True)
