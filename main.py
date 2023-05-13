63c5280937ed8321fef0c844d1cea10537caf314
Infinity4B <mail707293768@qq.com>
2023-05-13 12:00:50 +0800

    Add submit button

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ app = Flask(__name__, root_path=os.getcwd(),static_folder=os.path.join(os.getcwd
 
 @app.route('/')
 def mainpage():
-   tasks = config['tasks']
+   tasks = globalConfig['tasks']
    return render_template('./index.html',tasks=tasks)
 
 @app.route('/<task>/')
 def rating(task):
    return redirect(f'/{task}/1')
 
-@app.route('/<task>/submit',methods=['POST'])
-def submit(task):
-   return redirect('/')
+@app.route('/<task>/<num>/submit',methods=['POST'])
+def submit(task,num):
+   totalNum = globalConfig['tasks'][task]['totalNum']
+   print('a')
+   if int(num) > totalNum:
+      return redirect(f'/{task}/{totalNum}')
+   else:
+      return redirect(f'/{task}/{num+1}')
 
 @app.route('/<task>/jump',methods=['POST'])
 def direct(task):
-   taskConfig = config['tasks'][task]
+   taskConfig = globalConfig['tasks'][task]
+   print(taskConfig)
    totalNum = taskConfig['totalNum']
    if request.method == 'POST':
       num = request.form['num']
@@ -34,9 +40,9 @@ def direct(task):
 
 @app.route('/<task>/<num>')
 def rating_n(task, num):
-   taskConfig = config['tasks'][task]
+   taskConfig = globalConfig['tasks'][task]
    totalNum = taskConfig['totalNum']
-   pageMax = totalNum // config['settings']['indexPerPage']
+   pageMax = totalNum // globalConfig['settings']['indexPerPage']
    page = int(request.args.get('page', 1))
    loopMax = totalNum if page*36 > totalNum else page*36
    with open(os.path.join(os.getcwd(),taskConfig['datasetPath']),'r') as f:
@@ -50,5 +56,5 @@ def rating_n(task, num):
 
 if __name__ == '__main__':
    with open('config.yml','r') as f:
-      config = yaml.load(f, yaml.FullLoader)
-   app.run(debug=True)
+      globalConfig = yaml.load(f, yaml.FullLoader)
+      app.run(debug=True)
